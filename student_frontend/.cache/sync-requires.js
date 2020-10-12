c0d043952ce32e0ec7149c4f2cc0a7df15439e76
mig-25 <you@example.com>
2020-10-12 10:56:42 +0200

    make file test

--- student_frontend/.cache/sync-requires.js
+++ student_frontend/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/sohailhasware/GitHub/deploy-test/student_frontend/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/sohailhasware/GitHub/deploy-test/student_frontend/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/sohailhasware/GitHub/deploy-test/student_frontend/src/pages/about.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/Users/sohailhasware/GitHub/deploy-test/student_frontend/src/pages/blog.js"))),
